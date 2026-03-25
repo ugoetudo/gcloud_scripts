7926743ce0e30d6eb7907982cb20a86eff7847f5
ugoetudo <uoe12341234@gmail.com>
2026-03-25 19:23:36 -0400

    remove passwords

--- encrypt_passwords.sh
+++ encrypt_passwords.sh
@@ -4,7 +4,7 @@ echo "Ent3r_the_metastore" | gcloud kms encrypt \
 --key=hive-db-key-v1 \
 --plaintext-file=- \
 --ciphertext-file=hive-password.encrypted
-echo "W00ble\$R00ble\$" | gcloud kms encrypt \
+echo "your password here" | gcloud kms encrypt \
 --location=global \
 --keyring=etudo-bda-keyring \
 --key=hive-db-key-v1 \
